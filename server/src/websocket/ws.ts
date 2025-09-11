5543f73acfca879ff9ff4f7d893408de4486de10
Anton M. <antonmenshickov@gmail.com>
2025-09-11 14:35:04 +0700

    use uuid for event message id generation

--- server/src/websocket/ws.ts
+++ server/src/websocket/ws.ts
@@ -2,14 +2,13 @@ import { NextFunction, Request } from 'express';
 import { WebSocket } from 'ws';
 import { v4 as uuidv4 } from 'uuid';
 import { ReliableBatchQueue } from '../eventsQueue/reliableBatchQueue';
-let index = 0;
+
 export function websocket(ws: WebSocket, req: Request, next: NextFunction): void {
   ws.onmessage = (event) => {
     const logMessage = JSON.parse(event.data as string);
     const eventData = {
       ...logMessage,
-      // id: uuidv4()
-      id: `${index++}`
+      id: uuidv4()
     }
     ReliableBatchQueue.instance.enqueue(eventData);
     ws.send(`Echo: ${event.data}`);
