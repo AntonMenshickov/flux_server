e293b1b52ab2851e02f68d58f9f92b6f9f346694
Anton M. <antonmenshickov@gmail.com>
2025-10-19 11:24:07 +0700

    Add config load error to log.

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,11 @@ import dotenv from 'dotenv';
 //Reading config from config.env
 const environmentConfig = path.join(__dirname, '/config.env');
 console.log('Loading config from file', environmentConfig);
-dotenv.config({ path: environmentConfig })
+const configLoadResult = dotenv.config({ path: environmentConfig })
+
+if (configLoadResult.error) {
+  console.error(configLoadResult.error);
+}
 
 
 import { startServer } from './app';
