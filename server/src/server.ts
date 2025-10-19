dd3d2f5fbf54ef5d3a113a641c0bd93ddb47c2c3
Anton M. <antonmenshickov@gmail.com>
2025-10-19 11:19:18 +0700

    Add config path to log.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 //Reading config from config.env
 const environmentConfig = path.join(__dirname, '/config.env');
+console.log('Loading config from file', environmentConfig);
 dotenv.config({ path: environmentConfig })
 
 
